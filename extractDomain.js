ae7c680a35a712b833bbc1e98cb36b534ed297ee
Alexander Scherer <alexscherer@outlook.com>
2020-04-20 11:08:05 +0200

    Change extractDomain.js

--- extractDomain.js
+++ extractDomain.js
@@ -4,14 +4,19 @@ function domainName (url) {
   //your code here
 
   for (let i = 0; i < url.length; i++) {
-    if (url[i] == "/" && url[i + 1]) {
+    if (url[i] == "/" && url[i + 1] == "/") {
       url = url.substring(i + 2);
     }
-    else if (url[i] == "w" && url[i + 1] == "w" && url[i + 2] == "w" && url[i + 3] == ".") {
+
+  }
+
+  for (let i = 0; i < url.length; i++) {
+    if (url[i] == "w" && url[i + 1] == "w" && url[i + 2] == "w" && url[i + 3] == ".") {
       url = url.substring(i + 4);
     }
 
   }
+
   for (let i = 0; i < url.length; i++) {
     if (url[i] == ".") {
       url = url.substring(0, i);
@@ -20,5 +25,14 @@ function domainName (url) {
 
   console.log("url: ", url);
   return url;
-
 }
+
+
+/*
+function domainName(url){
+  url = url.replace("https://", '');
+  url = url.replace("http://", '');
+  url = url.replace("www.", '');
+  return url.split('.')[0];
+};
+*/
